aec07d72e487a51b0b55d0bb3e1244b481ea707b
chlduq04 <chlduq04@gmail.com>
2014-09-20 23:28:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -69,3 +69,66 @@ Object.extend = function(destination, source) {
     }
     return destination;
 };
+
+/**----------------**/
+
+function jquery_extends(){
+	if(jQuery){
+		$.fn.extend({
+			bind_clk_toggle : function(on_func, off_func){
+				var self = this;
+				this.swt = false;
+				$(this).bind("click",function(){
+					this.swt = !this.swt;
+					if(this.swt){
+						on_func(this);
+					}else{
+						off_func(this);
+					}
+				})
+			},
+			bind_clk_toggles : function(){
+				var self = this;
+				this.swt = false;
+				this.argus = arguments;
+				return (function(target, argu){
+					target.bind("click",function(){
+						if(this.swt == undefined || this.swt+1 == argu.length){
+							this.swt = 0;
+						}else{
+							this.swt++;
+						}
+						argu[this.swt](target);
+					})
+				})(self, arguments)
+			},
+			on_clk_toggle : function(on_func, off_func){
+				var self = this;
+				this.swt = false;
+				$(this).on("click",function(){
+					this.swt = !this.swt;
+					if(this.swt){
+						on_func(this);
+					}else{
+						off_func(this);
+					}
+				})
+			},
+			on_clk_toggles : function(){
+				var self = this;
+				this.swt = false;
+				this.argus = arguments;
+				return (function(target, argu){
+					target.on("click",function(){
+						if(this.swt == undefined || this.swt+1 == argu.length){
+							this.swt = 0;
+						}else{
+							this.swt++;
+						}
+						argu[this.swt](target);
+					})
+				})(self, arguments)
+			},
+		})
+	};
+}
