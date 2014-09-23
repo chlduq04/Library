06ca3f2d79c300b4ad90ab37fe60813a268f8438
chlduq04 <chlduq04@gmail.com>
2014-09-23 19:17:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -155,3 +155,71 @@ function clickMouseLong(){
 		})
 	}
 }
+
+/**---------------------------- check lock switch -----------------------------**/
+
+function check_lock(switch, func){
+	return function(param){
+		if(switch){
+			func(param);
+		}
+	}
+}
+/*
+ex)
+function something(){
+	$(".find").bind("click",check_lock(true, function(e){
+		//...
+	}));
+}
+*/
+
+
+/**----------------------------- easy ajax call -------------------------------**/
+
+function ajax_call( type, url, data, datatype, cache, ifsuccess, iferror ){
+	$.ajax({
+		type : type,
+		url : base_url+url,
+		data : JSON.stringify(data),
+		dataType : datatype,
+		cache : cache,
+		success : function(data) {
+			ifsuccess(data);
+		},
+		error : function(e){
+			iferror(e);
+		}
+	});		
+}
+
+function easy_ajax_call( url, data, ifsuccess, iferror ){
+	$.ajax({
+		type : "POST",
+		url : base_url+url,
+		data : JSON.stringify(data),
+		dataType : "json",
+		cache : false,
+		success : function(data) {
+			ifsuccess(data);
+		},
+		error : function(e){
+			iferror(e);
+		}
+	});		
+}
+
+function easy_get_ajax_call( url, ifsuccess, iferror ){
+	$.ajax({
+		type : "GET",
+		url : base_url+url,
+		dataType : "json",
+		cache : false,
+		success : function(data) {
+			ifsuccess(data);
+		},
+		error : function(e){
+			iferror(e);
+		}
+	});		
+}
